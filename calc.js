64c2d3c7cf68b7fdebea9b47cea7450a3ec9f79a
Matt Pusch <mpusch88@gmail.com>
2019-02-15 21:07:41 -0700

    test

---
+++ calc.js
@@ -0,0 +1,7 @@
+leftPar = document.getElementById('(')
+list_of_elements = []
+
+button_element.addEventListener('click', function() {
+	list_of_elements.push(test)
+	console.log(list_of_elements)
+});
