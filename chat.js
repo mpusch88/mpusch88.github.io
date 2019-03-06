faabea0ca0f84a809257b0673e2dbdcd32e81091
Matt Pusch <mpusch88@gmail.com>
2019-03-06 10:33:07 -0700

    update

--- chat.js
+++ chat.js
@@ -31,192 +31,187 @@ flag = true;
 display.innerText = current
 
 leftPar.addEventListener("click", function() {
-    current = append(current, '(')
-    display.innerText = current
+	current = append(current, '(')
+	display.innerText = current
 });
 
 rightPar.addEventListener('click', function() {
-    current = append(current, ')')
-    display.innerText = current
+	current = append(current, ')')
+	display.innerText = current
 });
 
 add.addEventListener('click', function() {
-	if(flag)
-	{
+	if (flag) {
 		flag = !flag
 	}
-    current = append(current, '+')
-    display.innerText = current
+	current = append(current, '+')
+	display.innerText = current
 });
 
 sub.addEventListener('click', function() {
-	if(flag)
-	{
+	if (flag) {
 		flag = !flag
 	}
-    current = append(current, '-')
-    display.innerText = current
+	current = append(current, '-')
+	display.innerText = current
 });
 
 mult.addEventListener('click', function() {
-	if(flag)
-	{
+	if (flag) {
 		flag = !flag
 	}
-    current = append(current, '*')
-    display.innerText = current
+	current = append(current, '*')
+	display.innerText = current
 });
 
 div.addEventListener('click', function() {
-	if(flag)
-	{
+	if (flag) {
 		flag = !flag
 	}
-    current = append(current, '/')
-    display.innerText = current
+	current = append(current, '/')
+	display.innerText = current
 });
 
 clr.addEventListener('click', function() {
-    current = 0
-    display.innerText = current
+	current = 0
+	display.innerText = current
 });
 
 equals.addEventListener('click', function() {
-    current = eval(current)
-    last = current
-    flag = true
-    display.innerText = current
+	current = eval(current)
+	last = current
+	flag = true
+	display.innerText = current
 });
 
 ans.addEventListener('click', function() {
-    if (last != null) {
-        current = append(current, last)
-        flag = !flag
-    }
-    display.innerText = current
+	if (last != null) {
+		current = append(current, last)
+		flag = !flag
+	}
+	display.innerText = current
 });
 
 dec.addEventListener('click', function() {
-	if(flag)
-	{
+	if (flag) {
 		flag = !flag
 	}
-    current = append(current, '.')
-    display.innerText = current
+	current = append(current, '.')
+	display.innerText = current
 });
 
 zero.addEventListener('click', function() {
-    if (flag) {
-        current = append(current, 0)
-        flag = !flag
+	if (flag) {
+		current = append(current, 0)
+		flag = !flag
 
-    }
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 one.addEventListener('click', function() {
-    if (flag) {
-        current = 1
-        flag = !flag
-    } else {
-        current = append(current, 1);
-    }
+	if (flag) {
+		current = 1
+		flag = !flag
+	} else {
+		current = append(current, 1);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 two.addEventListener('click', function() {
-    if (flag) {
-        current = 2
+	if (flag) {
+		current = 2
 
-        flag = !flag
-    } else {
-        current = append(current, 2);
-    }
+		flag = !flag
+	} else {
+		current = append(current, 2);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 three.addEventListener('click', function() {
-    if (flag) {
-        current = 3
-        flag = !flag
-    } else {
-        current = append(current, 3);
-    }
+	if (flag) {
+		current = 3
+		flag = !flag
+	} else {
+		current = append(current, 3);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 four.addEventListener('click', function() {
-    if (flag) {
-        current = 4
+	if (flag) {
+		current = 4
 
-        flag = !flag
+		flag = !flag
 
-    } else {
-        current = append(current, 4);
-    }
+	} else {
+		current = append(current, 4);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 five.addEventListener('click', function() {
-    if (flag) {
-        current = 5
-        flag = !flag
-    } else {
-        current = append(current, 5);
-    }
+	if (flag) {
+		current = 5
+		flag = !flag
+	} else {
+		current = append(current, 5);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 six.addEventListener('click', function() {
-    if (flag) {
-        current = 6
-        flag = !flag
-    } else {
-        current = append(current, 6);
-    }
+	if (flag) {
+		current = 6
+		flag = !flag
+	} else {
+		current = append(current, 6);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 seven.addEventListener('click', function() {
-    if (flag) {
-        current = 7
-        flag = !flag
-    } else {
-        current = append(current, 7);
-    }
+	if (flag) {
+		current = 7
+		flag = !flag
+	} else {
+		current = append(current, 7);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 eight.addEventListener('click', function() {
-    if (flag) {
-        current = 8
-        flag = !flag
-    } else {
-        current = append(current, 8);
-    }
+	if (flag) {
+		current = 8
+		flag = !flag
+	} else {
+		current = append(current, 8);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 nine.addEventListener('click', function() {
-    if (flag) {
-        current = 9
-        flag = !flag
-    } else {
-        current = append(current, 9);
-    }
+	if (flag) {
+		current = 9
+		flag = !flag
+	} else {
+		current = append(current, 9);
+	}
 
-    display.innerText = current
+	display.innerText = current
 });
 
 function append(a, b) {
-    return a.toString() + b.toString();
+	return a.toString() + b.toString();
 }
